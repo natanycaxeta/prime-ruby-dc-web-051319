1ce796ba160ac3b18800bc67fdafc820b24e4e2b
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 15:07:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,13 @@
-def prime?(num)
-  return if num <= 1
-  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+#def prime?(num)
+#  return if num <= 1
+#  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+#end
+def prime?(n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
