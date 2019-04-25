e10ea5739d6cf76373df2448a7cfe934c35fbc21
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 15:03:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,4 @@
 def is_prime?(num)
-  return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
